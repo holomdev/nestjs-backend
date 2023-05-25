78607acc9f52cff1b4de00c3d1a438c1113be45f
holomdev <holom.dev@gmail.com>
2023-05-25 10:19:55 -0600

    add sign up method to authentication service

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashingService } from '../hashing/hashing.service';
+import { SignUpDto } from './dto/sign-up.dto';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto) {
+    try {
+      const user = new User();
+      user.email = signUpDto.email;
+      user.password = await this.hashingService.hash(signUpDto.password);
+
+      await this.userRepository.save(user);
+    } catch (err) {
+      const pgUniqueViolationErrorCode = '23505';
+      if (err.code === pgUniqueViolationErrorCode) {
+        throw new ConflictException();
+      }
+
+      throw err;
+    }
+  }
+}
