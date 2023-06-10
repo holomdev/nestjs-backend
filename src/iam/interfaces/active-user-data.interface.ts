dc19b352d9c90b191fc3da929e97c4566d29c614
holomdev <holom.dev@gmail.com>
2023-06-10 12:11:36 -0600

    claims based authorization

--- src/iam/interfaces/active-user-data.interface.ts
+++ src/iam/interfaces/active-user-data.interface.ts
@@ -1,8 +1,13 @@
 import { Role } from '../../users/enums/role.enum';
+import { PermissionType } from '../authorization/permission.type';
 export interface ActiveUserData {
   sub: number;
 
   email: string;
 
   role: Role;
+
+  // Nota: Puedes usar role o permission, segun te convenga.
+  // Usar los dos no es tan logico
+  permissions: PermissionType[];
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,9 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { Role } from '../enums/role.enum';
+import {
+  Permission,
+  PermissionType,
+} from '../../iam/authorization/permission.type';
 
 @Entity()
 export class User {
@@ -14,4 +18,9 @@ export class User {
 
   @Column({ enum: Role, default: Role.Regular })
   role: Role;
+
+  // Nota: No tiene caso tener "role" si usamos permisos, solo se dejo
+  // para ejemplificacion de como se implementa.
+  @Column({ enum: Permission, default: [], type: 'json' })
+  permissions: PermissionType[];
 }
